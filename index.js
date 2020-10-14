dd55d1d594d98973696e49104fddd502b6c92766
outsider1113 <56751350+outsider1113@users.noreply.github.com>
2020-10-13 18:18:01 -0700

    lmao
    dawd

---
+++ index.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var god;
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  
+});
+
+
+client.on('message', msg => {
+    //node let VC = msg.member.voice.channel.members.each(user => console.log(user.id));
+    //console.log(VC);
+  if (msg.content.startsWith("`")) {
+    if(msg.content === "`move"){
+  }
+  msg.guild.roles.fetch("763466233374048296")
+  .then(role =>{ god = role.members.array()
+    //console.log(god[6].user.id)
+    
+   for(i= 0; i < god.length; i++){
+console.log(god[i].user )
+let dude = god[i];
+if(dude.voice.channel){
+    dude.voice.setChannel("740721179249082539")
+}
+//dude.voice.setChannel("740721179249082539")
+   }
+
+   msg.reply("The scripters have been moved. ")
+
+
+
+
+  })
+  .catch(console.error);  
+  
+  
+    }
+  
+
+
+});
+
+client.login(process.env.TOKEN);
